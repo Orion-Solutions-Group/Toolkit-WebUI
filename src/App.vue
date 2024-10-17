034c6b79f82fe292a8b1d313695e214c1806f8d1
RousseauFlorian <florian.rousseau@epitech.eu>
2024-10-17 10:45:59 +0200

    feature: add Modal for Orion Component

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,35 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
+import { ref } from 'vue';
+import OrionModal from './components/OrionModal.vue';
+
+const showModal = ref(false);
+
+const handleAccept = () => {
+  showModal.value = false;
+  console.log('Modal accepted');
+  console.log("showModal", showModal.value);
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="showModal = true" class="btn-primary">Open Modal</button>
+  <div>
+    <OrionModal
+        :open="showModal"
+        title="Supprimer un client ?"
+        message="Voulez-vous vraiment supprimer ce client ? Cette action est irréversible."
+        confirmText="Accepter"
+        cancelText="Annuler"
+        @accept="handleAccept"
+        @update:open="handleAccept"
+        size="lg"
+    />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/declarations.d.ts
@@ -0,0 +1,5 @@
+declare module '@heroicons/vue/*' {
+    import { defineComponent } from 'vue';
+    const icon: ReturnType<typeof defineComponent>;
+    export default icon;
+}
