6b0e0111e4631a86aac00300dc41147c61833335
Vladislav Razyvika <vladislav.razyvika@gmail.com>
2024-10-31 14:53:26 +0100

    feat: add first version of badge with marging and padding

--- src/components/OrionModal.vue
+++ src/components/OrionModal.vue
@@ -1,18 +1,36 @@
 <template>
-  <div v-if="isOpen" class="min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50" id="modal-id">
+  <div
+    v-if="isOpen"
+    class="min-w-screen h-screen animated fadeIn faster fixed left-0 top-0 flex justify-center items-center inset-0 z-50"
+    id="modal-id"
+  >
     <div class="absolute bg-black opacity-50 inset-0 z-0"></div>
-    <div :class="modalSizeClass" class="p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white">
+    <div
+      :class="modalSizeClass"
+      class="p-5 relative mx-auto my-auto rounded-xl shadow-lg bg-white"
+    >
       <div>
         <div class="text-center p-5 flex-auto justify-center">
-          <component :is="iconComponent" class="w-8 h-8 flex items-center mx-auto" :class="iconColorClass" />
+          <component
+            :is="iconComponent"
+            class="w-8 h-8 flex items-center mx-auto"
+            :class="iconColorClass"
+          />
           <h2 class="text-xl font-bold py-4">{{ title }}</h2>
           <p class="text-sm text-gray-500 px-8">{{ message }}</p>
         </div>
         <div class="p-3 mt-2 text-center space-x-4 md:block">
-          <button @click="closeModal" class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100">
+          <button
+            @click="closeModal"
+            class="mb-2 md:mb-0 bg-white px-5 py-2 text-sm shadow-sm font-medium tracking-wider border text-gray-600 rounded-full hover:shadow-lg hover:bg-gray-100"
+          >
             {{ cancelText }}
           </button>
-          <button @click="confirmAction" :class="confirmButtonClass" class="mb-2 md:mb-0 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg">
+          <button
+            @click="confirmAction"
+            :class="confirmButtonClass"
+            class="mb-2 md:mb-0 px-5 py-2 text-sm shadow-sm font-medium tracking-wider text-white rounded-full hover:shadow-lg"
+          >
             {{ confirmText }}
           </button>
         </div>
@@ -33,7 +51,8 @@ export default defineComponent({
     },
     message: {
       type: String,
-      default: 'This is the regular modal with supporting text below as additional content.',
+      default:
+        'This is the regular modal with supporting text below as additional content.',
     },
     cancelText: {
       type: String,
@@ -41,7 +60,7 @@ export default defineComponent({
     },
     confirmText: {
       type: String,
-      default: "Okay, got it",
+      default: 'Okay, got it',
     },
     open: {
       type: Boolean,
@@ -68,10 +87,10 @@ export default defineComponent({
     const isOpen = ref(props.open);
 
     watch(
-        () => props.open,
-        (newValue) => {
-          isOpen.value = newValue;
-        }
+      () => props.open,
+      (newValue) => {
+        isOpen.value = newValue;
+      }
     );
 
     const closeModal = () => {
---
+++ src/components/TestBadge.vue
@@ -0,0 +1,83 @@
+<script setup lang="ts">
+import { computed, defineProps } from 'vue';
+
+const props = defineProps({
+  label: {
+    type: String,
+    required: true,
+  },
+  margin: {
+    type: String,
+    default: '', // ex: 'my-2'
+  },
+  padding: {
+    type: String,
+    default: 'px-4 py-2', // ex: 'px-4 py-2'
+  },
+  color: {
+    type: String,
+    default: 'bg-blue-500',
+  },
+  size: {
+    type: String,
+    default: 'text-sm',
+    validator(val: string) {
+      return ['text-xs', 'text-sm', 'text-lg'].includes(val);
+    },
+  },
+  variant: {
+    type: String,
+    default: 'full',
+    validator(value: string) {
+      return ['full', 'outline'].includes(value);
+    },
+  },
+  leftIcon: {
+    type: String,
+    required: false,
+  },
+  rightIcon: {
+    type: String,
+    required: false,
+  },
+  dot: {
+    type: Boolean,
+    default: false,
+  },
+  clickable: {
+    type: Boolean,
+    default: false,
+  },
+  visible: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const badgeClass = computed(() => {
+  const baseClasses = [
+    'inline-flex items-center rounded-full', // Coins arrondis pour le badge
+    props.padding,
+    props.size,
+    props.clickable ? 'cursor-pointer hover:bg-opacity-75 transition' : '',
+    props.variant === 'outline'
+      ? `border ${props.color.replace('bg', 'border')} ${props.color.replace(
+        'bg',
+        'text'
+      )} bg-transparent`
+      : `${props.color} text-white`,
+  ];
+
+  return baseClasses.join(' ');
+});
+</script>
+
+<template>
+  <div v-if="visible" :class="[badgeClass, margin]">
+    <span v-if="dot" class="w-2 h-2 rounded-full bg-red-500 mr-2"></span>
+    <i v-if="leftIcon" :class="[leftIcon, 'mr-2']"></i>
+    <!-- Directement afficher le label ici -->
+    <span>{{ label }}</span>
+    <i v-if="rightIcon" :class="[rightIcon, 'ml-2']"></i>
+  </div>
+</template>
