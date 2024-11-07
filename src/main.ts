b949a0aae085477f97142c1ff037588ca2435984
RousseauFlorian <florian.rousseau@epitech.eu>
2024-11-07 11:39:57 +0100

    feat: add toaster service to re-use this from another web app

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,11 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import './assets/main.css'
+import 'vue-toast-notification/dist/theme-bootstrap.css';
+import ToastPlugin from "vue-toast-notification";
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(ToastPlugin);
 
 app.mount('#app')
